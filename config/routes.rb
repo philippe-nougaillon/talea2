89dd82b46b8582bd4e20a94b1b5f6cd1310a8ed5
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2024-04-20 18:31:56 +0200

    google fonts symbol replace svg icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'admin/audits'
+
   devise_for :users, controllers: { 
     registrations: 'users/registrations'
   }
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
       get :archiver
     end
   end
+
+  get 'admin/audits'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -32,5 +35,6 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "interventions#index"
+
 end
