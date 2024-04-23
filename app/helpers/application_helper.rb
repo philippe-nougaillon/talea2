44e6b1ff932e897388ce4bc89b5bd3eb33646ec2
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-04-23 10:45:25 +0200

    prettify small changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,14 +13,14 @@ module ApplicationHelper
           when 'Integer'
             pretty_changes << "#{key} initialisé à '#{User.find(ids).nom_prénom}'"
           when 'Array'
-            pretty_changes << "#{key} changé de '#{User.find_by(id: ids.first).try(:nom_prénom) || "id n°#{ids.first} (Utilisateur supprimé)"}' à '#{User.find_by(id: ids.last).try(:nom_prénom) || "id n°#{ids.last} (Utilisateur supprimé)"}'"
+            pretty_changes << "#{key} changé de '#{User.find_by(id: ids.first).try(:nom_prénom) || "#{ids.first} (Utilisateur supprimé)"}' à '#{User.find_by(id: ids.last).try(:nom_prénom) || "#{ids.last} (Utilisateur supprimé)"}'"
           end 
         else
           case ids.class.name
           when 'NilClass'
-            pretty_changes << "#{key} vide"
+            pretty_changes << "#{key} initialisé à vide"
           when 'Array'
-            pretty_changes << "#{key.pluralize} supprimés (id n°#{ids.join(' et ')})"
+            pretty_changes << "#{key} changé de #{ids.join(' à ')} (Utilisateurs supprimés))"
           when 'Integer'
             pretty_changes << "#{key} initialisé à #{ids} (Utilisateur supprimé)"
           end
