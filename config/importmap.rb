00fac2523c25b2e5fc8db030fc0721cdf3803263
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-04-30 12:30:11 +0200

    PWA V2

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-notification" # @2.2.0
 pin "hotkeys-js" # @3.13.7
 pin "stimulus-use" # @0.51.3
+pin_all_from "app/javascript/custom", under: "custom"
--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/service-worker.js" => "service_worker#service_worker"
+  get "/manifest.json" => "service_worker#manifest"
+
   # Defines the root path route ("/")
   root "interventions#index"
 
