e0eb3615d0cee1fffff2571858925dbc355323ea
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-04-30 15:03:43 +0200

    stimulus textarea_autogrow added

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "stimulus-notification" # @2.2.0
 pin "hotkeys-js" # @3.13.7
 pin "stimulus-use" # @0.51.3
 pin_all_from "app/javascript/custom", under: "custom"
+pin "stimulus-textarea-autogrow" # @4.1.0
