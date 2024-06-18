e540e1fcee30d97e2b944203dca6c7c680649280
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-06-18 14:29:54 +0200

    audits : user filter added

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,9 +10,18 @@ class AdminController < ApplicationController
     @audits = @organisation_audits.order("id DESC")
     @types  = @organisation_audits.pluck(:auditable_type).uniq.sort
     @actions= %w[update create destroy]
+    @users = current_user.organisation.users.ordered 
+
+    if params[:search].present?
+      @audits = @audits.where("audited_changes ILIKE ?", "%#{params[:search]}%")
+    end
 
     if params[:start_date].present? && params[:end_date].present? 
       @audits = @audits.where("created_at BETWEEN (?) AND (?)", params[:start_date], params[:end_date])
+    end
+
+    if params[:user_id].present?
+      @audits = @audits.where(user_id: params[:user_id])
     end
 
     if params[:type].present?
@@ -21,10 +30,6 @@ class AdminController < ApplicationController
 
     if params[:action_name].present?
       @audits = @audits.where(action: params[:action_name])
-    end
-
-    if params[:search].present?
-      @audits = @audits.where("audited_changes ILIKE ?", "%#{params[:search]}%")
     end
 
     @pagy, @audits = pagy(@audits)
