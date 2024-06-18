fee84a6c8eed11f5e0d89ad41f33fe34cf8419e2
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-06-18 14:09:19 +0200

    user show : audits added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @audits = Audited::Audit.where(user_id: @user.id).reorder(id: :desc)
   end
 
   # GET /users/new
