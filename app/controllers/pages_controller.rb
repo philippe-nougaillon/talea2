2ffdc0f0570c977ce3c858b7b78e1ba573e88b15
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-06-27 10:06:00 +0200

    assistant : UX / wording

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,20 @@
 class PagesController < ApplicationController
   before_action :is_user_authorized
   def assistant
-    llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
+    if params[:commit].present?
+      llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
 
-    interventions = current_user.organisation.interventions.where.not(début: nil).order(:début)
+      interventions = current_user.organisation.interventions.where.not(début: nil).order(:début)
 
-    description_list = []
-    interventions.each do |intervention|
-      description_list << "#{intervention.description.gsub('[mail] ', '')} #{l(intervention.début.to_date)}"
+      description_list = []
+      interventions.each do |intervention|
+        description_list << "#{intervention.description.gsub('[mail] ', '')} #{l(intervention.début.to_date)}"
+      end
+
+      #TODO faire un prompt avec un textfield qui boucle sur cette route
+
+      @results = llm.chat(messages: [{role: "user", content: "Génère moi des nouvelles tâches en te basant sur cette liste : #{description_list.join(', ')}"}]).completion
     end
-
-    #TODO faire un prompt avec un textfield qui boucle sur cette route
-
-    @results = llm.chat(messages: [{role: "user", content: "Génère moi des nouvelles tâches en te basant sur cette liste : #{description_list.join(', ')}"}]).completion
   end
 
   private
