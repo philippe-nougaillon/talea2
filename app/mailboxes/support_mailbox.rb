052a3f6764a6d7dcba74cc552e81d0d54ccad16f
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-10-01 16:42:04 +0200

    mailboxes : puts removed / code reworking

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,16 +1,34 @@
 class SupportMailbox < ApplicationMailbox
 
   def process
-    raise
     organisation_id = nil
     from_email = mail.from_address.to_s.split('<').last.split('>').first
-    puts "email : #{from_email}"
-    puts 'avant check adhérent/manager'
+    # puts "email : #{from_email}"
+    # puts 'avant check adhérent/manager'
     # Chercher si l'envoyeur est un adhérent ou un manager
     if user = User.where(rôle: [0,2]).find_by(email: from_email)
-      puts 'pendant check adhérent/manager'
+      # puts 'pendant check adhérent/manager'
       organisation_id = user.organisation_id
-      puts "organisation_id : #{organisation_id}"
+      # puts "organisation_id : #{organisation_id}"
+
+      # puts 'après check adhérent/manager et avant check organisation_id'
+
+      if organisation_id
+        # puts 'pendant check organisation_id'
+        organisation = Organisation.find(organisation_id)
+        # puts "organisation : #{organisation.inspect}"
+        intervention = organisation.interventions.new( 
+                        adherent_id: user.id, 
+                        user_id: user.id,
+                        description: "[MAIL] #{mail.subject}", 
+                        commentaires: "De #{mail.from_address} : #{mail.body}"
+                      )
+        # puts "#{intervention.inspect}"
+        # puts "#{intervention.valid?}"
+        intervention.save
+        # puts 'après création intervention'
+      end
+      # puts 'après check organisation_id'
     else
       # Organisation.all.each do |organisation|
       #   if mail.subject.include?(organisation.numero)
@@ -18,21 +36,6 @@ class SupportMailbox < ApplicationMailbox
       #   end
       # end
     end
-    puts 'après check adhérent/manager et avant check organisation_id'
-
-    if organisation_id
-      puts 'pendant check organisation_id'
-      organisation = Organisation.find(organisation_id)
-      puts "organisation : #{organisation.inspect}"
-      organisation.interventions.create( 
-                      adherent_id: user.try(:id), 
-                      description: "[MAIL] #{mail.subject.gsub(organisation.numero, '')}", 
-                      commentaires: "De #{mail.from_address} : #{mail.body}",
-                      user_id: user.try(:id)
-                    )
-        puts 'après création intervention'
-    end
-    puts 'après check organisation_id'
   end
 
 end
